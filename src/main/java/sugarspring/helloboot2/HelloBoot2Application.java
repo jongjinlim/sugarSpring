a8483416562e26373e1d18434cf3ac02790c100d
jjlim <jongjin826@gmail.com>
2024-12-02 15:38:19 +0900

    [spring] 서블릿 컨테이너 띄우기 ServletWebServerFactory 를 이용해서 띄워봤다

--- src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
+++ src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
@@ -1,13 +1,17 @@
 package sugarspring.helloboot2;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 @SpringBootApplication
 public class HelloBoot2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloBoot2Application.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();	// 이게 진짜 웹서버
+		webServer.start();
 	}
 
 }
