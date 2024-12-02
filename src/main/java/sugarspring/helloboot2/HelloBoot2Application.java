6f5a0bdebdb6ae748e5d95b9688a4ba047d1cb9a
jjlim <jongjin826@gmail.com>
2024-12-02 15:31:04 +0900

    project init

---
+++ src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
@@ -0,0 +1,13 @@
+package sugarspring.helloboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBoot2Application.class, args);
+	}
+
+}
