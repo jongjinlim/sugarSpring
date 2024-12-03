6a90e4ff5c9cc7a8b7542777b7e665bf09825584
jjlim <jongjin826@gmail.com>
2024-12-03 17:19:10 +0900

    [spring] 합성 어노테이션의 적용

--- src/main/java/sugarspring/helloboot2/Config.java
+++ src/main/java/sugarspring/helloboot2/Config.java
@@ -1,17 +1,20 @@
 package sugarspring.helloboot2;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-
-@MySpringBootAnnotation
-public class HelloBoot2Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HelloBoot2Application.class, args);
+@Configuration
+public class Config {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
 	}
 
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
 }
