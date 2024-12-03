12cc2bce288c6316e0fb3b1e2e21e14f2b7cdf39
jjlim <jongjin826@gmail.com>
2024-12-03 14:03:32 +0900

    [spring] SpringBooatApplication으로 전환

--- src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
+++ src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
@@ -1,12 +1,11 @@
 package sugarspring.helloboot2;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -24,24 +23,7 @@ public class HelloBoot2Application {
 	}
 
 	public static void main(String[] args) {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//				dispatcherServlet.setApplicationContext(this);
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-						.addMapping("/*");
-				});
-				webServer.start();
-			}
-		};
-
-		applicationContext.register(HelloBoot2Application.class);
-		applicationContext.refresh();	// 초기화 작업
+		SpringApplication.run(HelloBoot2Application.class, args);
 	}
 
 }
