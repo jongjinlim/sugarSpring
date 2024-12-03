68068cc96e6298629f7c19c0b54249a2c603465d
jjlim <jongjin826@gmail.com>
2024-12-03 16:37:37 +0900

    [spring] DI와 단위 테스트

--- src/main/java/sugarspring/helloboot2/HelloController.java
+++ src/main/java/sugarspring/helloboot2/HelloController.java
@@ -10,17 +10,18 @@ import java.util.Objects;
 public class HelloController {
 
 	private final HelloService helloService;
-	private final ApplicationContext applicationContext;
+//	private final ApplicationContext applicationContext;
 
-	public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+	public HelloController(HelloService helloService) {
 		this.helloService = helloService;
-		this.applicationContext = applicationContext;
-
-		System.out.println(applicationContext);
+//		System.out.println(applicationContext);
 	}
 
 	@GetMapping("/hello")
 	public String hello(String name) {
-		return helloService.sayHello(Objects.requireNonNull(name));	// null 체크까지
+		if(name == null || name.trim().length() == 0) {
+			throw new IllegalArgumentException();
+		}
+		return helloService.sayHello(name);	// null 체크까지
 	}
 }
--- src/test/java/sugarspring/helloboot2/HelloApiTest.java
+++ src/test/java/sugarspring/helloboot2/HelloApiTest.java
@@ -24,4 +24,19 @@ public class HelloApiTest {
 		// body Hello Spring
 		assertThat(res.getBody()).isEqualTo("Hello Spring");
 	}
+
+	@Test
+	void failsHelloApi() {
+		TestRestTemplate rest = new TestRestTemplate();
+
+		ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+
+		// 응답 검증
+		// status code 200
+		assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+		// header(content-type) htext/plain
+//		assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+		// body Hello Spring
+//		assertThat(res.getBody()).isEqualTo("Hello Spring");
+	}
 }
