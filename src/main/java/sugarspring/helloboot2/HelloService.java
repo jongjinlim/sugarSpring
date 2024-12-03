fa8e13ddfeba812d53fdcf87c8bb399accd02860
jjlim <jongjin826@gmail.com>
2024-12-03 13:41:47 +0900

    [spring] @Component 스캔

--- src/main/java/sugarspring/helloboot2/HelloService.java
+++ src/main/java/sugarspring/helloboot2/HelloService.java
@@ -1,5 +1,8 @@
 package sugarspring.helloboot2;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface HelloService {
 	String sayHello(String name);
 }
--- src/main/java/sugarspring/helloboot2/SimpleHelloService.java
+++ src/main/java/sugarspring/helloboot2/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package sugarspring.helloboot2;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService {
 	@Override
 	public String sayHello(String name) {
