b5e10d40decd202d183ebcfbeac276f17fdc0dcf
jjlim <jongjin826@gmail.com>
2024-12-03 16:52:52 +0900

    [spring] DI를 이용한 Decorator, Proxy 패턴

--- src/main/java/sugarspring/helloboot2/HelloController.java
+++ src/main/java/sugarspring/helloboot2/HelloController.java
@@ -1,20 +1,15 @@
 package sugarspring.helloboot2;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
 
 @RestController
 public class HelloController {
 
 	private final HelloService helloService;
-//	private final ApplicationContext applicationContext;
 
 	public HelloController(HelloService helloService) {
 		this.helloService = helloService;
-//		System.out.println(applicationContext);
 	}
 
 	@GetMapping("/hello")
