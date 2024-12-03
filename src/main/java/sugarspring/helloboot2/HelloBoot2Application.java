f5c257fc1e9d23e9dd1e39e3825bbbbe70abf551
jjlim <jongjin826@gmail.com>
2024-12-03 13:18:10 +0900

    [spring] 자바코드 구성 정보 사용

--- src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
+++ src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
@@ -1,17 +1,27 @@
 package sugarspring.helloboot2;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@SpringBootApplication
+@Configuration
 public class HelloBoot2Application {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -25,8 +35,8 @@ public class HelloBoot2Application {
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+
+		applicationContext.register(HelloBoot2Application.class);
 		applicationContext.refresh();	// 초기화 작업
 	}
 
