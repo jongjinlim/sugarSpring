0d50264624de3b9c52bac7fc8bba18f5381b6095
jjlim <jongjin826@gmail.com>
2024-12-04 23:37:55 +0900

    [spring] ㅈㅏ동 구성에 Environment 프로퍼티 적용
    application.properties 에 테스트하고 VMOption, 환경변수 이 셋 중에 VMOption이 제일 우선순쉬 상위

--- src/main/java/sugarspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/sugarspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import sugarspring.config.ConditionalMyOnClass;
 import sugarspring.config.MyAutoConfiguration;
 
@@ -12,7 +13,9 @@ import sugarspring.config.MyAutoConfiguration;
 public class TomcatWebServerConfig {
 	@Bean("tomcatWebServerFactory")
 	@ConditionalOnMissingBean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
+	public ServletWebServerFactory servletWebServerFactory(Environment env) {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.setContextPath(env.getProperty("contextPath"));
+		return factory;
 	}
 }
--- src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
+++ src/main/java/sugarspring/helloboot2/HelloBoot2Application.java
@@ -1,11 +1,23 @@
 package sugarspring.helloboot2;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import sugarspring.config.MySpringBootApplication;
 
 
 @MySpringBootApplication
 public class HelloBoot2Application {
+
+//	@Bean
+//	ApplicationRunner applicationRunner(Environment env) {
+//		return args -> {
+//			String name = env.getProperty("my.name");
+//			System.out.println("my.name: " + name);
+//		};
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloBoot2Application.class, args);
