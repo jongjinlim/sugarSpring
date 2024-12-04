d209f13ae2872bd2b772b1f2468dcb49bff74e55
jjlim <jongjin826@gmail.com>
2024-12-04 16:50:22 +0900

    [spring] @Conditional 과 Condition

--- src/main/java/sugarspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/sugarspring/config/autoconfig/JettyWebServerConfig.java
@@ -3,12 +3,24 @@ package sugarspring.config.autoconfig;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import sugarspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
 	@Bean("jettyWebServerFactory")
 	public ServletWebServerFactory servletWebServerFactory() {
 		return new JettyServletWebServerFactory();
+	}
+
+	static class JettyCondition implements Condition {
+		@Override
+		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+			return true;
+		}
 	}
 }
