66b963fe52d31f9a2c9ceecb585dc182babc0271
jjlim <jongjin826@gmail.com>
2024-12-04 14:33:08 +0900

    [spring] 자동 구성 어노테이션 적용

--- src/main/java/sugarspring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/sugarspring/config/autoconfig/DispatcherServletConfig.java
@@ -3,8 +3,9 @@ package sugarspring.config.autoconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import sugarspring.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 	@Bean
 	public DispatcherServlet dispatcherServlet() {
