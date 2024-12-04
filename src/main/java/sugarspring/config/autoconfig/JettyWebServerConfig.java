571642f813e3787f574fea36fec840d242db1caa
jjlim <jongjin826@gmail.com>
2024-12-04 15:14:12 +0900

    [spring] 스타터와 Jetty 서버 구성 추가

--- src/main/java/sugarspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/sugarspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,14 @@
 package sugarspring.config.autoconfig;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import sugarspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-	@Bean("tomcatWebServerFactory")
+public class JettyWebServerConfig {
+	@Bean("jettyWebServerFactory")
 	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
+		return new JettyServletWebServerFactory();
 	}
 }
