d0754e346228fe38280e3e16fd28402f602cb12d
jjlim <jongjin826@gmail.com>
2024-12-05 14:54:31 +0900

    [spring] JdbcTemplate 과 트랜잭션 매니저 구성

--- src/main/java/sugarspring/config/autoconfig/DataSourceConfig.java
+++ src/main/java/sugarspring/config/autoconfig/DataSourceConfig.java
@@ -2,9 +2,12 @@ package sugarspring.config.autoconfig;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import sugarspring.config.ConditionalMyOnClass;
 import sugarspring.config.EnableMyConfigurationProperties;
 import sugarspring.config.MyAutoConfiguration;
@@ -15,6 +18,7 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
 
 	@Bean
@@ -38,5 +42,19 @@ public class DataSourceConfig {
 		dataSource.setUsername(properties.getPassword());
 		dataSource.setPassword(properties.getPassword());
 		return dataSource;
+	}
+
+	@Bean
+	@ConditionalOnSingleCandidate(DataSource.class)
+	@ConditionalOnMissingBean
+	JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean
+	@ConditionalOnSingleCandidate(DataSource.class)
+	@ConditionalOnMissingBean
+	JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+		return new JdbcTransactionManager(dataSource);
 	}
 }
---
+++ src/test/java/sugarspring/helloboot2/JdbcTemplateTest.java
@@ -0,0 +1,27 @@
+package sugarspring.helloboot2;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
+
+@HellobootTest
+public class JdbcTemplateTest {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@BeforeEach
+	void init() {
+		jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS HELLO(NAME VARCHAR(50) PRIMARY KEY, COUNT INT)");
+	}
+
+	@Test
+	void insertAndQuery() {
+		jdbcTemplate.update("INSERT INTO HELLO VALUES(?,?)", "sugar", 3);
+		jdbcTemplate.update("INSERT INTO HELLO VALUES(?,?)", "spring", 1);
+		Long count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM HELLO", Long.class);
+		Assertions.assertThat(count).isEqualTo(2);
+	}
+}
